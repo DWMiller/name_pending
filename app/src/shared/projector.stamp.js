68ec981ec48afb89700b0dfbb04877c8fed69e09
David Miller <davidabc66@gmail.com>
2016-02-01 17:19:56 -0500

    stuff

--- app/src/shared/projector.stamp.js
+++ app/src/shared/projector.stamp.js
@@ -3,10 +3,21 @@ function createProjectorStamp() {
         refs: {
             color: '#00FF00',
             scale: config.objects.projector.scale
+        },
+        methods: {
+            draw: function (ctx) {
+                ctx.strokeStyle = this.color;
+
+                ctx.beginPath();
+                ctx.moveTo(this.x, this.y);
+
+                ctx.lineTo(this.x * 2, this.y * 2);
+                ctx.stroke();
+            }
         }
     });
 
-    projector = stampit.compose(stamps.particle, projector, stamps.drawable);
+    projector = stampit.compose(stamps.particle, stamps.drawable, projector);
 
     return projector;
 }
